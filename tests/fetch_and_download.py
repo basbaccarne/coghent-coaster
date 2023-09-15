02b308e56907604801583a699f98755e7780d11a
Bas Baccarne <bastiaan.baccarne@ugent.be>
2023-09-15 09:43:48 +0200

    cleanup libs

--- tests/fetch_and_download.py
+++ tests/fetch_and_download.py
@@ -1,11 +1,9 @@
 # this test tests the data_fetcher & object fetcher
+from src.fetcher import *
 
-# libraries
-import sys
-sys.path.append('../src')
-from fetcher import *
-
-# Get image and save as data/image1.jpg
+# Get random image information
 new_object = data_fetcher()
+
+# If we get a result, download that image as data/image1.jpg
 if new_object is not None:
     img_fetcher(new_object, counter=1, action='download')
